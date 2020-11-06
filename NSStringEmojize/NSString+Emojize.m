82f71e07f2123f2a12626d55e55d5fbe13a39445
Lara Vertlberg <lara@firefly.agency>
2020-11-06 13:55:04 +0100

    Add url detector to check if there is an emoji into an URL

--- NSStringEmojize/NSString+Emojize.m
+++ NSStringEmojize/NSString+Emojize.m
@@ -9,6 +9,12 @@
 #import "NSString+Emojize.h"
 #import "emojis.h"
 
+BOOL NSRangeIntersectsRange(NSRange range1, NSRange range2) {
+    if (range1.location > range2.location + range2.length) return NO;
+    if (range2.location > range1.location + range1.length) return NO;
+    return YES;
+}
+
 @implementation NSString (Emojize)
 
 - (NSString *)emojizedString
@@ -20,19 +26,33 @@
 {
     static dispatch_once_t onceToken;
     static NSRegularExpression *regex = nil;
+    static dispatch_once_t dDetector = 0;
+    static NSDataDetector *urlDetector = nil;
     dispatch_once(&onceToken, ^{
         regex = [[NSRegularExpression alloc] initWithPattern:@"(:[a-z0-9-+_]+:)" options:NSRegularExpressionCaseInsensitive error:NULL];
     });
     
+    dispatch_once(&dDetector, ^{
+        urlDetector = [[NSDataDetector alloc] initWithTypes:NSTextCheckingTypeLink error:nil];
+    });
+    
     __block NSString *resultText = text;
     NSRange matchingRange = NSMakeRange(0, [resultText length]);
+    NSArray<NSTextCheckingResult *> *urlMatches = [urlDetector matchesInString:text options:NSMatchingReportCompletion range:matchingRange];
     [regex enumerateMatchesInString:resultText options:NSMatchingReportCompletion range:matchingRange usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
          if (result && ([result resultType] == NSTextCheckingTypeRegularExpression) && !(flags & NSMatchingInternalError)) {
              NSRange range = result.range;
              if (range.location != NSNotFound) {
+                 BOOL rangesIntersects = NO;
+                 for (NSTextCheckingResult *urlMatch in urlMatches) {
+                     rangesIntersects = NSRangeIntersectsRange(urlMatch.range, range);
+                     if (rangesIntersects) {
+                         break;
+                     }
+                 }
                  NSString *code = [text substringWithRange:range];
                  NSString *unicode = self.emojiAliases[code];
-                 if (unicode) {
+                 if (unicode && !rangesIntersects) {
                      resultText = [resultText stringByReplacingOccurrencesOfString:code withString:unicode];
                  }
              }
